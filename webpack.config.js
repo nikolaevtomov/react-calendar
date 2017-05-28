ad4732e6aa088d181d8b37bae6eb752f4764058e
Nikolaev Tomov <nikolaev.tomov@crunch.co.uk>
2017-05-28 20:28:11 +0100

    Optimising for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-var webpack = require('webpack')
-var path = require('path')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var CompressionPlugin = require('compression-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-var BUILD_ENV = process.env.NODE_ENV
-var DEPLOYMENT_ENV = process.env.DEPLOYMENT_ENV || 'local'
-var HOST = process.env.CLIENT_HOST || '0.0.0.0'
-var PORT = process.env.CLIENT_PORT || 3000
+const BUILD_ENV = process.env.NODE_ENV
+const DEPLOYMENT_ENV = process.env.DEPLOYMENT_ENV || 'local'
+const HOST = process.env.CLIENT_HOST || '0.0.0.0'
+const PORT = process.env.CLIENT_PORT || 3000
 
-var isDev = BUILD_ENV !== 'production'
+const isDev = BUILD_ENV !== 'production'
 
-// console.log('NODE_ENV:', process.env.NODE_ENV)
+// console.log('BUILD_ENV', BUILD_ENV)
 
-module.exports = {
+const config = {
 
   entry: [
     'babel-polyfill',
@@ -39,33 +40,21 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: isDev ? [
-          { loader: 'style-loader', options: {} },
-          { loader: 'css-loader', options: {} },
-          { loader: 'postcss-loader', options: {} }
-        ]
+        use: isDev
+        ? [ 'style-loader', 'css-loader', 'postcss-loader' ]
         : ExtractTextPlugin.extract([
-          { loader: 'style-loader', options: {} },
-          { loader: 'css-loader', options: {} },
-          { loader: 'postcss-loader', options: {} }
-        ])
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { minimize: true } },
+          { loader: 'postcss-loader' }
+        ]),
+        exclude: /(node_modules)/
       },
       {
         test: /\.scss$/,
-        use: isDev ? [
-          { loader: 'style-loader', options: {} },
-          { loader: 'css-loader', options: {} },
-          { loader: 'postcss-loader', options: {} },
-          { loader: 'sass-loader', options: {} }
-        ]
-        : ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: {} },
-            { loader: 'postcss-loader', options: {} },
-            { loader: 'sass-loader', options: {} }
-          ]
-        })
+        use: isDev
+        ? [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ]
+        : ExtractTextPlugin.extract({ use: [ 'css-loader', 'postcss-loader', 'sass-loader' ] }),
+        exclude: /(node_modules)/
       },
       { test: /\.html$/, use: ['html-loader'] },
       {
@@ -76,9 +65,7 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|svg)$/,
         use: {
           loader: 'url-loader',
-          options: {
-            limit: 100000
-          }
+          options: { limit: 100000 }
         }
       }
     ]
@@ -123,7 +110,14 @@ module.exports = {
       '__ENVIRONMENT__': JSON.stringify(BUILD_ENV),
       'process.env.NODE_ENV': JSON.stringify(BUILD_ENV),
       '__DEPLOYMENT_ENV__': JSON.stringify(DEPLOYMENT_ENV)
-    }),
+    })
+
+  ]
+
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
 
     new webpack.optimize.UglifyJsPlugin({
       mangle: true,
@@ -131,11 +125,16 @@ module.exports = {
         warnings: false,
         pure_getters: true,
         unsafe: true,
-        unsafe_comps: true,
+        // unsafe_comps: true,
         screw_ie8: true
       },
       output: { comments: false },
       exclude: [/\.min\.js$/gi]
+    }),
+
+    new OptimizeCssAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } }
     }),
 
     new CompressionPlugin({
@@ -146,6 +145,7 @@ module.exports = {
       minRatio: 0
     })
 
-  ]
-
+  )
 }
+
+module.exports = config
