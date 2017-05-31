e3def48183f7aa10717fe91d3c27a9a4e7553ee3
Nikolaev Tomov <nikolaev.tomov@crunch.co.uk>
2017-05-31 12:50:42 +0100

    forgot hashing

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: isDev ? 'bundle.js' : 'bundle.js',
+    filename: isDev ? 'bundle.js' : 'bundle.[hash].js',
     publicPath: '/'
   },
 
